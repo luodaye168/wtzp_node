7ed1b5d5fd93e850de603dc10cffe44c0d38ca97
luodaye168 <1621710565@qq.com>
2022-04-12 19:21:25 +0800

    修改mysql为内网地址

--- db/index.js
+++ db/index.js
@@ -2,7 +2,7 @@ const mysql = require('mysql')
 
 const db = mysql.createPool({
     host: '10.0.224.15',
-    port:'27151',
+    port:'3306',
     user: 'root',
     password:'1621710565@qq.COM',
     database:'my_db_01'
