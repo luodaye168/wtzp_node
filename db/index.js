50c6ab6047c6e216cece45f332e1dacb59f1b11e
luodaye168 <74521309+luodaye168@users.noreply.github.com>
2022-11-30 21:36:34 +0800

    Update index.js

--- db/index.js
+++ db/index.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql')
 
 const db = mysql.createPool({
-    host: '10.0.224.15',
+    host: '10.13.106.227',
     port:'3306',
     user: 'root',
-    password:'1621710565@qq.COM',
+    password:'LLll1817258884',
     database:'my_db_01'
 })
 
