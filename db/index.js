ac357730131f5c5bf7efcd8525222b6527fdc84d
luodaye168 <74521309+luodaye168@users.noreply.github.com>
2022-11-30 22:33:54 +0800

    Update index.js

--- db/index.js
+++ db/index.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql')
 
 const db = mysql.createPool({
-    host: 'sh-cynosdbmysql-grp-rvttdmpg.sql.tencentcdb.com', //'10.13.106.227'
-    port:'26904',//3306
+    host: '10.13.106.227', //'sh-cynosdbmysql-grp-rvttdmpg.sql.tencentcdb.com'
+    port:'3306',//26904
     user: 'root',
-    password:'LLll1817258884',
+    password:'LLll18172588884',
     database:'my_db_01'
 })
 
